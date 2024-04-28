8f4d794d609d5b02f9720ae362f17b870d493da2
Cherry6628 <162563824+Cherry6628@users.noreply.github.com>
2024-04-28 19:44:14 +0530

    Create 13) Java Application for Generic Max Finder.java

---
+++ 13) Java Application for Generic Max Finder.java
@@ -0,0 +1,45 @@
+import java.io.*; import java.util.*;
+public class GenericMax
+{
+public static void main(String[] args) {
+    Integer[] numbers = {1, 2, 3};
+    System.out.println(max(numbers));
+
+    String[] words = {"red", "green", "blue"};
+    System.out.println(max(words));
+
+    Circle[] circles = {new Circle(3), new Circle(2.9), new Circle(5.9)};
+    System.out.println(max(circles));
+}
+
+static class Circle implements Comparable<Circle> { //Circle object implements Comparable to compare with Circle
+double radius;
+public Circle (double radius)
+{
+this.radius = radius; //sets value of radius from main method for radius
+}
+@Override
+public int compareTo(Circle c)
+{
+if (radius < c.radius) return -1;
+else if (radius == c.radius) return 0;
+else
+return 1;
+}
+@Override
+public String toString()
+{
+return "Circle radius: " + radius;
+}
+}
+@SuppressWarnings("unchecked")
+public static <T extends Comparable<T>> T max(T... elements) {
+    T max = elements[0];
+    for (T element : elements) {
+        if (element.compareTo(max) > 0) {
+            max = element;
+        }
+    }
+    return max;
+}
+}
